98b2aead11f93cec2ab63f0652ad8dd009cfa426
shondarzertaroka95 <shondarzertaroka95@gmail.com>
2025-01-23 11:44:03 +0600

    types of order added

--- src/types/Order.ts
+++ src/types/Order.ts
@@ -1,5 +1,22 @@
 
 
-export interface IOrder{
-    
+export interface IOrder {
+
+    user: string;
+    orderItems: Array<{
+        name: string;
+        qty: number;
+        price: number;
+        product: string;
+    }>;
+    shippingAddress: string;
+    paymentMethod: string;
+    totalPrice: number;
+    isPaid: boolean;
+    paidAt?: Date;
+    isDelivered: boolean;
+    deliveredAt?: Date;
+
+
+
 }
